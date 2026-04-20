eec6973faf27f201cd894a633127cf2a911d2010
Nazar <self.coft@gmail.com>
2026-04-20 15:47:19 +0300

    Initialize logger configuration in main.py for improved logging

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,10 @@
 from litestar import Litestar
 from litestar.config.cors import CORSConfig
 from src.core.config import config
+from src.core.logger import configure_logger
 from src.router.endpoint import router
+
+configure_logger()
 
 app = Litestar(
     route_handlers=[router],
